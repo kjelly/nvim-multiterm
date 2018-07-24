7026a955310e85d8dbb3765a01bc803b5557416d
Kuo-tung Kao <kjellytw@gmail.com>
2018-07-24 23:38:12 +0800

    Refactor code

--- rplugin/python3/multiterm.py
+++ rplugin/python3/multiterm.py
@@ -1,5 +1,6 @@
 import re
 import neovim
+import enum
 try:
     import psutil
 except ImportError:
@@ -8,6 +9,12 @@ except ImportError:
 
 def isNumber(x):
     return x in '1234567890'
+
+
+class Result(enum.Enum):
+    BY_PASS = 1
+    HANDLED = 2
+    UNHANDLED = 3
 
 
 @neovim.plugin
@@ -54,6 +61,103 @@ class MultiTerm(object):
             elif len(val) == 2 and val[0] == '@':
                 args[i] = self.nvim.eval('@' + val[1])
 
+    def subcommand_a(self, arg0, args, range):
+        cmd = ' '.join(args[1:]) + '\n'
+        self.run_in_all_terminal(cmd)
+        return Result.HANDLED
+
+    def subcommand_s(self, arg0, args, range):
+        if len(arg0) == 2 and arg0[0] == 's' and isNumber(arg0[1]):
+            cmd = ' '.join(args[1:]) + '\n'
+            self.command_map[arg0[1]] = cmd
+            return Result.HANDLED
+        return Result.UNHANDLED
+
+    def subcommand_r(self, arg0, args, range):
+        if arg0[0] == 'r' and len(arg0) == 1:
+            self.echo(arg0)
+            cmd = self.command_map.get(arg0[1], '')
+            self.run(self.last_term_job_id, cmd)
+            return Result.HANDLED
+        elif arg0[0] == 'r' and len(arg0) == 2 and isNumber(arg0[1]):
+            # C g1 : run command 1 stored in command map.
+            self.echo(arg0)
+            cmd = self.command_map.get(arg0[1], '')
+            self.run(self.last_term_job_id, cmd)
+            return Result.HANDLED
+        return Result.UNHANDLED
+
+    def subcommand_n(self, arg0, args, range):
+        if arg0 in ['n', 'N'] and len(args) > 1:
+            if len(args) == 2:
+                try:
+                    filename = self.nvim.eval("expand('%:p')").split(',')[0]
+                    self.nvim.command("file %s,%s" % (filename, args[1]))
+                except:
+                    pass
+                self.name_map[self.last_term_job_id] = args[1]
+                return Result.HANDLED
+            elif len(args) > 2:
+                self.name_map[args[2]] = args[1]
+                return Result.HANDLED
+        return Result.UNHANDLED
+
+    def subcommand_g(self, arg0, args, range):
+        name_or_id = args[1]
+        inv_name_map = {v: k for k, v in self.name_map.items()}
+        inv_data_map = {v: k for k, v in self.data.items()}
+        r = inv_name_map.get(name_or_id, None)
+        if r is None:
+            r = name_or_id
+        r = inv_data_map.get(r, None)
+        if r is None:
+            return Result.BY_PASS
+        self.nvim.command("buffer %s" % r)
+        return Result.HANDLED
+
+    def subcommand_w(self, arg0, args, range):
+        if psutil is None:
+            return Result.BY_PASS
+        inv_name_map = {v: k for k, v in self.name_map.items()}
+        inv_data_map = {v: k for k, v in self.data.items()}
+        if inv_name_map.get('w3m', None) is None:
+            self.nvim.command("terminal")
+            self.nvim.command("C n w3m")
+        inv_name_map = {v: k for k, v in self.name_map.items()}
+        inv_data_map = {v: k for k, v in self.data.items()}
+        job_id = inv_name_map['w3m']
+        file_name = inv_data_map[job_id]
+        self.nvim.command("buffer %s" % file_name)
+        url = ' '.join(args[1:]) + '\n'
+        pid = file_name.split('/')[-1].split(':')[0]
+        p = psutil.Process(pid=int(pid, 10))
+        childrens = p.children()
+        if len(childrens) > 0 and childrens[0].name() == 'w3m':
+            childrens[0].kill()
+        self.run(job_id, '%s %s' % (self.browser, url))
+        self.nvim.command("normal! i")
+        return Result.HANDLED
+
+    def subcommand_l(self, arg0, args, range):
+        # C l : list all terminal.
+        if len(arg0) > 1:
+            return Result.UNHANDLED
+        text = ''
+        for i in self.data:
+            job_id = self.data[i]
+            text += '%s => %s, %s\n' % (job_id, i,
+                                        self.name_map.get(job_id, ''))
+        try:
+            job_id = self.nvim.eval('expand(b:terminal_job_id)')
+        except:
+            pass
+        text += 'current job_id=%s, name=%s' % (job_id, self.name_map[job_id])
+        self.echo(text)
+        return Result.HANDLED
+
+    def subcommand_empty(self, arg0, args, range):
+        return Result.UNHANDLED
+
     @neovim.command("C", range='', nargs='*', sync=True)
     def command(self, args, range):
         if len(args) < 1:
@@ -66,84 +170,25 @@ class MultiTerm(object):
 
         self.replace_args(args)
 
+        function_map = {
+            'a': self.subcommand_a,
+            'g': self.subcommand_g,
+            'l': self.subcommand_l,
+            'n': self.subcommand_n,
+            'r': self.subcommand_r,
+            's': self.subcommand_s,
+            'w': self.subcommand_w,
+        }
+
         arg0 = args[0]
-        if arg0 in ['a', 'A']:
-            # C a ls : Run ls in all terminal
-            cmd = ' '.join(args[1:]) + '\n'
-            self.run_in_all_terminal(cmd)
-        elif len(arg0) == 2 and arg0[0] == 's' and isNumber(arg0[1]):
-            # C s1 ls : store command `ls` in command map 1.
-            cmd = ' '.join(args[1:]) + '\n'
-            self.command_map[arg0[1]] = cmd
-        elif arg0[0] == 'r' and len(arg0) == 1:
-            # C g : show all commmand stored in command map.
-            text = ''
-            for i in self.command_map:
-                text += '%s => %s' % (i, self.command_map[i])
-            self.echo(text)
-        elif arg0[0] == 'r' and len(arg0) == 2 and isNumber(arg0[1]):
-            # C g1 : run command 1 stored in command map.
-            self.echo(arg0)
-            cmd = self.command_map.get(arg0[1], '')
-            self.run(self.last_term_job_id, cmd)
 
-        elif arg0 in ['n', 'N'] and len(args) > 1:
-            if len(args) == 2:
-                try:
-                    filename = self.nvim.eval("expand('%:p')").split(',')[0]
-                    self.nvim.command("file %s,%s" % (filename, args[1]))
-                except:
-                    pass
-                self.name_map[self.last_term_job_id] = args[1]
-            elif len(args) > 2:
-                self.name_map[args[2]] = args[1]
+        result = function_map.get(arg0[0],
+                                  self.subcommand_empty)(arg0, args, range)
 
-        elif arg0 in ['g', 'G'] and len(args) > 1:
-            name_or_id = args[1]
-            inv_name_map = {v: k for k, v in self.name_map.items()}
-            inv_data_map = {v: k for k, v in self.data.items()}
-            r = inv_name_map.get(name_or_id, None)
-            if r is None:
-                r = name_or_id
-            r = inv_data_map.get(r, None)
-            if r is None:
-                return
-            self.nvim.command("buffer %s" % r)
-        elif arg0 in ['w', 'W']:
-            if psutil is None:
-                return
-            inv_name_map = {v: k for k, v in self.name_map.items()}
-            inv_data_map = {v: k for k, v in self.data.items()}
-            if inv_name_map.get('w3m', None) is None:
-                self.nvim.command("terminal")
-                self.nvim.command("C n w3m")
-            inv_name_map = {v: k for k, v in self.name_map.items()}
-            inv_data_map = {v: k for k, v in self.data.items()}
-            job_id = inv_name_map['w3m']
-            file_name = inv_data_map[job_id]
-            self.nvim.command("buffer %s" % file_name)
-            url = ' '.join(args[1:]) + '\n'
-            pid = file_name.split('/')[-1].split(':')[0]
-            p = psutil.Process(pid=int(pid, 10))
-            childrens = p.children()
-            if len(childrens) > 0 and childrens[0].name() == 'w3m':
-                childrens[0].kill()
-            self.run(job_id, '%s %s' % (self.browser, url))
-            self.nvim.command("normal! i")
+        if result == Result.BY_PASS or result == Result.HANDLED:
+            return
 
-        elif arg0 in ['l', 'L']:
-            # C l : list all terminal.
-            text = ''
-            for i in self.data:
-                job_id = self.data[i]
-                text += '%s => %s, %s\n' % (job_id, i, self.name_map.get(job_id, ''))
-            try:
-                job_id = self.nvim.eval('expand(b:terminal_job_id)')
-            except:
-                pass
-            text += 'current job_id=%s, name=%s' % (job_id, self.name_map[job_id])
-            self.echo(text)
-        elif re.match(r'(\d+,)*\d+', arg0):
+        if re.match(r'(\d+,)*\d+', arg0):
             # C 1, 3 ls : run ls in terminal 1, terminal 3.
             cmd = ' '.join(args[1:]) + '\n'
             for i in arg0.split(','):
@@ -161,7 +206,6 @@ class MultiTerm(object):
                 ever_run = True
             if ever_run is False:
                 self.run(self.last_term_job_id, cmd)
-
         else:
             cmd = ' '.join(args[:]) + '\n'
             self.run(self.last_term_job_id, cmd)
@@ -201,4 +245,3 @@ class MultiTerm(object):
                 self.nvim.command("normal! i")
         except:
             pass
-
